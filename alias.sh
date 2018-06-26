26af762d9e1af6864dcfc36bc22b3a7f47f7d268
Jordan Knight <jakkaj@gmail.com>
2018-06-26 11:30:08 +1000

    WIP

---
+++ alias.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#kubes
+alias kk='watch -n 3 kubectl get nodes,svc,pods'
+alias krun='kubectl run -i -t jordoutils --image=jakkaj/jordoutils:latest --restart=Never -- bash'
+alias kexec='kubectl exec -it jordoutils -- bash'
+alias kstop='kubectl stop jordoutils'
+
+
+#dockers
+alias dbuild='docker build -t jkbuild .'
+alias dstop='docker stop $(docker ps -q)'
+
+#helms
+alias hdry='helm install -f values.yaml --dry-run --debug .'
